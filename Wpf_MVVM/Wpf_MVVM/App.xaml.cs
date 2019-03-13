ef568d6fb94e3c5e5174972647b6e48ddf99e12f
CarlosMMartinez <48124370+CarlosMMartinez@users.noreply.github.com>
2019-03-13 22:02:54 +0100

    Update App.xaml.cs

--- Wpf_MVVM/Wpf_MVVM/App.xaml.cs
+++ Wpf_MVVM/Wpf_MVVM/App.xaml.cs
@@ -14,6 +14,8 @@ namespace Wpf_MVVM
     /// </summary>
     public partial class App : Application
     {
+    //Para no tener que listar en los distintos tipos de viewModels creamos 
+    // en al App este método y así no disponemos de código repetido.
         private static DbConnector _dbConnector = new DbConnector();
 
         public static DbConnector DbConnector { get => _dbConnector;  }
